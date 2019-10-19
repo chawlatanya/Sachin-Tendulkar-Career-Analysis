3982acbb0928368b5edd15f169855bd3c65991da
Tanya Chawla <chawlatanya31@gmail.com>
2019-10-19 12:39:48 +0530

    added box shadow to stats cards

--- src/components/stats.js
+++ src/components/stats.js
@@ -13,32 +13,32 @@ import React , {useState,useEffect} from 'react';
         .get("http://localhost:3001/get-stats")
         .then((response)=>{
             setTotal(response.data)
-        })
+        })   
     },[])
 
     return(
         <div style={{display:"flex",margin:"0 auto"}}>
-            <Card style={{margin:"20px",padding:"10px",backgroundColor:"#D4EDDA"}} border="success" body>
+            <Card style={{margin:"20px",padding:"10px",backgroundColor:"#D4EDDA","boxShadow": "0 4px 8px 0 rgba(0,0,0,0.2)"}} border="success" body>
                <center><strong>{total[3]}</strong></center>
                 Matches
             </Card>
-            <Card style={{margin:"20px",padding:"10px",backgroundColor:"#FFEEBA"}} border="warning" body>
+            <Card style={{margin:"20px",padding:"10px",backgroundColor:"#FFEEBA","boxShadow": "0 4px 8px 0 rgba(0,0,0,0.2)"}} border="warning" body>
             <center><strong>{total[0]}</strong></center>
                 Half-Centuries
             </Card>
-            <Card style={{margin:"20px",padding:"10px",backgroundColor:"#E2E3E5"}} border="secondary" body>
+            <Card style={{margin:"20px",padding:"10px",backgroundColor:"#E2E3E5","boxShadow": "0 4px 8px 0 rgba(0,0,0,0.2)"}} border="secondary" body>
             <center><strong> {total[1]}</strong></center>
                 Centuries
             </Card>
-            <Card style={{margin:"20px",padding:"10px",backgroundColor:"#CCE5FF"}} border="info" body>
+            <Card style={{margin:"20px",padding:"10px",backgroundColor:"#CCE5FF","boxShadow": "0 4px 8px 0 rgba(0,0,0,0.2)"}} border="info" body>
             <center><strong>{total[2]}</strong></center>
                 Double Century
             </Card>
-            <Card style={{margin:"20px",padding:"10px",backgroundColor:"#F8D7DA"}} border="danger" body>
+            <Card style={{margin:"20px",padding:"10px",backgroundColor:"#F8D7DA","boxShadow": "0 4px 8px 0 rgba(0,0,0,0.2)"}} border="danger" body>
             <center><strong> {total[5]}</strong></center>
                 Runs
             </Card>
-            <Card style={{margin:"20px",padding:"10px",backgroundColor:"#D6D8D9"}} border="dark" body>
+            <Card style={{margin:"20px",padding:"10px",backgroundColor:"#D6D8D9","boxShadow": "0 4px 8px 0 rgba(0,0,0,0.2)"}} border="dark" body>
             <center><strong> {total[4]}</strong></center>
                 Wickets
             </Card>
